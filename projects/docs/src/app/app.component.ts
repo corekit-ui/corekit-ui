2a97db767eb9334a66b9d89335df6d00009c35ed
Azamat R <azamat@rasulov.dev>
2024-08-10 16:13:13 +0500

    feat(components): accordion

--- projects/docs/src/app/app.component.ts
+++ projects/docs/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { CkH1 } from '@corekit/ui/typography'
+import { LucideIconConfig } from 'lucide-angular'
+import { AccordionPageComponent } from './features/components/accordion-page/accordion-page.component'
 import { TypographyPageComponent } from './features/components/typography-page/typography-page.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TypographyPageComponent, CkH1],
+  imports: [TypographyPageComponent, CkH1, AccordionPageComponent],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'block container space-y-6 py-6' }
+  host: { class: 'block container space-y-8 py-6' }
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private readonly _lucide: LucideIconConfig) {
+    this._lucide.size = 16
+  }
+}
---
+++ projects/ui/accordion/components/accordion-item-header/accordion-item-header.ts
@@ -0,0 +1,48 @@
+import { Component, booleanAttribute, input } from '@angular/core'
+import { classNames } from '@corekit/ui/utils'
+import { CkAccordionItem } from '../accordion-item/accordion-item'
+
+export const accordionItemHeader =
+  'py-3 gap-2 flex items-center w-full text-start font-semibold [&:is([disabled])]:opacity-50 [&:is([disabled])]:pointer-events-none'
+
+/**
+ * OnPush change detection is not possible in this case
+ * due to changes not picking up when another item toggled
+ * in single mode
+ */
+// eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
+@Component({
+  selector: 'ck-accordion-item-header, [ckAccordionItemHeader]',
+  exportAs: 'ckAccordionItemHeader',
+  standalone: true,
+  templateUrl: './accordion-item-header.html',
+  host: {
+    role: 'button',
+    '[class]': '_class',
+    '[tabindex]': '_accordionItem.disabled ? -1 : 0',
+    '[id]': "_accordionItem.id + '-header'",
+    '[attr.aria-controls]': '_accordionItem.id',
+    '[attr.disabled]': '_accordionItem.disabled ? "" : null',
+    '(click)': '_accordionItem.toggle()',
+    '(keydown.space)': '$event.preventDefault(); _accordionItem.toggle()',
+    '(keydown.enter)': '$event.preventDefault(); _accordionItem.toggle()',
+    '(keydown.arrowright)': '_accordionItem.open()',
+    '(keydown.arrowleft)': '_accordionItem.close()'
+  }
+})
+export class CkAccordionItemHeader {
+  public readonly class = input<string>()
+
+  /**
+   * Whether the expansion indicator should be hidden.
+   */
+  public readonly hideToggle = input<boolean, unknown>(false, {
+    transform: booleanAttribute
+  })
+
+  protected get _class(): string {
+    return classNames(accordionItemHeader, this.class())
+  }
+
+  constructor(protected readonly _accordionItem: CkAccordionItem) {}
+}
