f2c5aac42ec00df9be5e2a386b517239a8d59111
Azamat R <azamat@rasulov.dev>
2024-08-12 22:34:23 +0500

    feat(components): label

---
+++ projects/ui/label/label.ts
@@ -0,0 +1,15 @@
+import { Directive, input } from '@angular/core'
+import { classNames } from '@corekit/ui/utils'
+
+@Directive({
+  selector: '[ckLabel], label',
+  standalone: true,
+  host: { '[class]': '_class' }
+})
+export class CkLabel {
+  public readonly class = input<string>()
+
+  protected get _class(): string {
+    return classNames('font-medium peer-disabled:opacity-50', this.class())
+  }
+}
