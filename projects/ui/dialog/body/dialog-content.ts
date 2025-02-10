4ff1984dec776d51eb954122cd9e9b1458760320
Azamat R <azamat@rasulov.dev>
2025-02-10 20:37:56 +0500

    refactor: use computed signals for CSS classes string

--- projects/ui/dialog/body/dialog-content.ts
+++ projects/ui/dialog/body/dialog-content.ts
@@ -1,15 +1,13 @@
-import { Directive, input } from '@angular/core'
+import { computed, Directive, input } from '@angular/core'
 import { classNames } from '@corekit/ui/utils'
 
 @Directive({
   selector: '[ck-dialog-content], ck-dialog-content',
   standalone: true,
-  host: { '[class]': '_class' }
+  host: { '[class]': '_class' },
 })
 export class CkDialogContent {
   public readonly class = input<string>()
 
-  protected get _class(): string {
-    return classNames('block px-6', this.class())
-  }
+  protected _class = computed(() => classNames('block px-6', this.class()))
 }
--- projects/ui/dialog/body/dialog-subtitle.ts
+++ projects/ui/dialog/body/dialog-subtitle.ts
@@ -1,15 +1,15 @@
-import { Directive, input } from '@angular/core'
+import { computed, Directive, input } from '@angular/core'
 import { classNames } from '@corekit/ui/utils'
 
 @Directive({
   selector: '[ck-dialog-subtitle], ck-dialog-subtitle',
   standalone: true,
-  host: { '[class]': '_class' }
+  host: { '[class]': '_class()' },
 })
 export class CkDialogSubtitle {
   public readonly class = input<string>()
 
-  protected get _class(): string {
+  protected _class = computed(() => {
     return classNames('text-muted-foreground', this.class())
-  }
+  })
 }
